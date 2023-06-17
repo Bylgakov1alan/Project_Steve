01f4631e8e2bd0a9631d866daa469b08c65d7799
Alan <bulgakov.alan@list.ru>
2023-06-17 21:03:18 +0300

    alan_9.1

--- src/views/Ads/AdView.vue
+++ src/views/Ads/AdView.vue
@@ -1,14 +1,34 @@
 <template>
-  <v-container>
-    <v-row justify="center">
-      <h1>Ad</h1>
-    </v-row>
-  </v-container>
+    <v-container>
+        <v-row>
+            <v-col cols="12">
+                <v-card class="mt-5">
+                    <v-img height="300px" src="https://w.forfun.com/fetch/ef/efec5b0d09345e0a9d981b5b60d1880c.jpeg"></v-img>
+                    <v-card-text>
+                        <h1 class="text--primary mb-3">Steve</h1>
+                        <p>
+                            Основная задача игрока в игре Minecraft — выжить в блочном трехмерном мире, собирая ресурсы, такие как дерево, камень и
+                            еда, и используя эти ресурсы для строительства убежища, инструментов и других предметов. Игрок должен избегать опасных
+                            существ и опасностей, исследуя и добывая ценные материалы, такие как золото, алмазы и редкие руды. Игрок также может
+                            создавать оружие и доспехи для защиты от врагов и охотиться за едой, чтобы прокормить себя. Цель игры может
+                            варьироваться в зависимости от предпочтений игрока, но обычно она включает в себя строительство и создание структур,
+                            исследование мира и выживание в нем и, в конечном итоге, победу над последним боссом игры, Драконом Края.
+                        </p>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn class="warning" color="orange">Edit</v-btn>
+                        <v-btn class="success" color="green">Buy</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 <script>
 export default {
-  data() {
-    return {};
-  },
+    data() {
+        return {};
+    },
 };
 </script>
