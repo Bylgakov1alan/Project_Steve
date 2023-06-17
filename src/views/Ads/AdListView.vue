8c633f42270e156362ea3ebfc21a4eb77b1e0096
Alan <bulgakov.alan@list.ru>
2023-06-17 20:06:28 +0300

    alan_8.1

--- src/views/Ads/AdListView.vue
+++ src/views/Ads/AdListView.vue
@@ -1,14 +1,16 @@
 <template>
-  <v-container>
-    <v-row justify="center">
-      <h1>AdList</h1>
-    </v-row>
-  </v-container>
+    <v-container>
+        <v-row justify="center" sm6 offset-sm3>
+            <h1 class="text--secondary mb-3 mt-3">My ads</h1>
+        </v-row>
+    </v-container>
 </template>
+
 <script>
 export default {
-  data() {
-    return {};
-  },
-};
+    data() {
+        return {
+        }
+    }
+}
 </script>
