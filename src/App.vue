5791a17b577df3f53aa7c26d0d587733563bacc0
Alan <bulgakov.alan@list.ru>
2023-06-17 18:13:02 +0300

    alan_2.8

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
       </v-list-item>
       <v-divider></v-divider>
       <v-list dense>
-        <v-list-item v-for="link in links" :key="link.title">
+        <v-list-item v-for="link in links" :key="link.title" :to="link.url">
           <template v-slot:prepend>
             <v-icon :icon="link.icon"></v-icon>
           </template>
@@ -25,7 +25,7 @@
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn text v-for="link in links" :key="link.title">
+        <v-btn text v-for="link in links" :key="link.title" :to="link.url">
           <v-icon start :icon="link.icon"></v-icon>
           {{ link.title }}
         </v-btn>
