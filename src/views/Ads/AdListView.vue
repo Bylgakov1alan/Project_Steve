4fe90be2160cd692061382aa0bd8195ea91117ed
Alan <bulgakov.alan@list.ru>
2023-06-17 22:43:16 +0300

    alan_10.4

--- src/views/Ads/AdListView.vue
+++ src/views/Ads/AdListView.vue
@@ -1,7 +1,7 @@
 <template>
  <v-container>
   <h1 class="text--secondary mb-3 mt-3 ">My ads</h1>
-  <v-card v-for="ad in ads" :key="ad.id" class="mx-auto mb-5" max-width="1000">
+  <v-card v-for="ad in myAds" :key="ad.id" class="mx-auto mb-5" max-width="1000">
    <v-row class="ma-2">
     <v-img :src="ad.src" height="175px"></v-img>
     <v-spacer></v-spacer>
@@ -24,39 +24,10 @@
 </template>
 <script>
  export default {
-  data() {
-   return {
-    ads: [
-     {
-      title: "First",
-      desc: "First Desc",
-      promo: true,
-      src: "https://w.forfun.com/fetch/ef/efec5b0d09345e0a9d981b5b60d1880c.jpeg",
-      id: "1",
-     },
-     {
-      title: "Second",
-      desc: "Second Desc",
-      promo: true,
-      src: "https://i.ytimg.com/vi/zaCVh8Ue9rc/maxresdefault.jpg",
-      id: "2",
-     },
-     {
-      title: "Third",
-      desc: "Thitd Desc",
-      promo: true,
-      src: "https://gamerwall.pro/uploads/posts/2021-11/1637288547_46-gamerwall-pro-p-teksturi-mobov-v-mainkrafte-oboi-na-zastav-47.png",
-      id: "3",
-     },
-     {
-      title: "Fouth",
-      desc: "Fouth Desc",
-      promo: true,
-      src: "https://phonoteka.org/uploads/posts/2022-09/1663296783_3-phonoteka-org-p-ender-drakon-art-krasivo-3.jpg",
-      id: "4",
-     },
-    ],
-   };
-  },
+  computed: {
+   myAds() {
+    return this.$store.getters.myAds
+   }
+  }
  };
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,86 +1,56 @@
 <template>
-  <div>
-    <v-container fluid>
-      <v-layout row>
-        <v-flex xs12>
-          <v-carousel>
-            <v-carousel-item v-for="ad in ads" :key="ad.id" :src="ad.src">
-              <div class="ad-link">
-                <v-btn class="error" :to="'/ad/' + ad.id">
-                  {{ ad.title }}
-                </v-btn>
-              </div>
-            </v-carousel-item>
-          </v-carousel>
-        </v-flex>
-      </v-layout>
-    </v-container>
-    <v-container grid-list-lg>
-      <v-layout row wrap>
-        <v-flex xs12 sm6 md4 v-for="ad in ads" :key="ad.id">
-          <v-card>
-            <v-img :src="ad.src" height="200px"></v-img>
-            <v-card-title primary-title>
-              <div>
-                <h3 class="headline mb-0">{{ad.title}}</h3>
-                <div>{{ad.desc}}</div>
-              </div>
-            </v-card-title>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn text :to="'/ad/' + ad.id">
-                Open
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" xs="12">
+        <v-carousel>
+          <v-carousel-item v-for="ad in promoAds" :key="ad.id" :src="ad.src" cover>
+            <div class="ad-link">
+              <v-btn class="error" :to="'/ad/' + ad.id">
+                {{ ad.title }}
               </v-btn>
-              <v-btn raised color="primary">
-                Buy
-              </v-btn>
-            </v-card-actions>
-
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </div>
+            </div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-col>
+    </v-row>
+  </v-container>
+  <v-container grid-list-lg>
+    <v-row justify="center">
+      <v-col cols="12" sm="6" md="4" xs="12" v-for="ad in ads" :key="ad.id">
+        <v-card>
+          <v-img :src="ad.src" height="200px"></v-img>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb0">{{ ad.title }}</h3>
+              <div>{{ ad.desc }}</div>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text :to="'/ad/' + ad.id">Open</v-btn>
+            <v-btn raised color="primary">
+              Buy
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
 <script>
   export default {
-    data() {
-      return {
-        ads: [
-          {
-            title: "First",
-            desc: "First Desc",
-            promo: true,
-            src: "https://w.forfun.com/fetch/ef/efec5b0d09345e0a9d981b5b60d1880c.jpeg",
-            id: "1"
-          },
-          {
-            title: "Second",
-            desc: "Second Desc",
-            promo: true,
-            src: "https://i.ytimg.com/vi/zaCVh8Ue9rc/maxresdefault.jpg",
-            id: "2"
-          },
-          {
-            title: "Third",
-            desc: "Thitd Desc",
-            promo: true,
-            src: "https://gamerwall.pro/uploads/posts/2021-11/1637288547_46-gamerwall-pro-p-teksturi-mobov-v-mainkrafte-oboi-na-zastav-47.png",
-            id: "3"
-          },
-          {
-            title: "Fouth",
-            desc: "Fouth Desc",
-            promo: true,
-            src: "https://phonoteka.org/uploads/posts/2022-09/1663296783_3-phonoteka-org-p-ender-drakon-art-krasivo-3.jpg",
-            id: "4"
-          }
-
-        ]
+    computed: {
+      promoAds() {
+        return this.$store.getters.promoAds
+      },
+      ads() {
+        return this.$store.getters.ads
       }
     }
-  } 
+  }
 </script>
+
 <style scoped>
   .ad-link {
     position: absolute;
