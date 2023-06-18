eb42a191255f6430649d7a16d18d5aa682dcca9e
Alan <bulgakov.alan@list.ru>
2023-06-18 07:26:45 +0300

    alan_19.1

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,11 @@ import { createStore } from 'vuex'
 import ads from './ads'
 import user from './user'
 import shared from './shared'
-import order from './order'
+import orders from './orders'
 
 const store = createStore({
 modules:{
-    ads, user, shared, order
+    ads, user, shared, orders
 }
 })
 
